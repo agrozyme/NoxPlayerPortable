b56a13e16ad5ed4159b9c8cb88748bb61b0cf1b3
agrozyme <agrozyme@gmail.com>
2020-05-30 15:09:58 +0800

    cleanup code

--- README.md
+++ README.md
@@ -4,12 +4,19 @@ Fork form: https://github.com/BlueMagicalWolf/NoxPlayerPortable
 
 A script that will make a almost portable version of NoxPlayer (Windows Only)
 
-## How to run setup:
+## Tools
+
+all tools include in the repository
+
+- 7-Zip: https://www.7-zip.org/download.html
+- Batch-To-Exe-Converter: https://www.softpedia.com/get/System/File-Management/Batch-To-Exe-Converter.shtml
+
+## How to run setup
 
 - run `Setup.cmd`.
 - after the script complete running the folder noxplayer is ready to beign copied, you can copy that folder to whereever you want (like a usb).
 
-## How to launch:
+## How to launch
 
 to launch noxplayer, run the exe file in the noxplayer folder (normal you would use Nox.exe).
 
--- Setup.ps1
+++ Setup.ps1
@@ -27,67 +27,47 @@ function Pause([String] $Text = 'Press any key to continue ...') {
   [System.Console]::ReadKey($true)
 }
 
-class Setup {
-  [String] $NoxPlayerSetupUri = 'http://res06.bignox.com/full/20200525/309a2a31cac647e88653f03d8703830b.exe?filename=nox_setup_v6.6.0.8_full_intl.exe'
+function Main {
+  $NoxPlayerSetupUri = 'http://res06.bignox.com/full/20200525/309a2a31cac647e88653f03d8703830b.exe?filename=nox_setup_v6.6.0.8_full_intl.exe'
 
-  [String] GetNoxPlayerFolder() {
-    return $PSScriptRoot + '\NoxPlayer'
+  $AssetFolder = $PSScriptRoot + '\assets'
+  $Expand = $AssetFolder + '\7za.exe'
+  $Bignox = $AssetFolder + '\Bignox.7z'
+  $Converter = $AssetFolder + '\Bat_To_Exe_Converter.exe'
+  $CommandPattern = $AssetFolder + '\SecretRunScripts\*.cmd'
+
+  $NoxPlayerFolder = $PSScriptRoot + '\NoxPlayer'
+  $NoxPlayerSetupFile = $NoxPlayerFolder + '\NoxPlayerSetup.exe'
+  $NoxFolder = $NoxPlayerFolder + '\Nox'
+  $Icon = $NoxFolder + '\bin\res\shortcut5.ico'
+
+  Write-Host 'Creating NoxPlayer Folder...'
+  New-Item -ItemType Directory -Path $NoxPlayerFolder -Force
+
+  Write-Host 'Downloading NoxPlayerSetup Package...'
+  Receive-File $NoxPlayerSetupUri $NoxPlayerSetupFile
+
+  Write-Host 'Extracting NoxPlayerSetup Files...'
+  & $Expand x $NoxPlayerSetupFile -o"$NoxFolder" -y
+
+  Write-Host 'Extracting Bignox Files...'
+  & $Expand x $Bignox -o"$NoxPlayerFolder" -y
+
+  Write-Host 'Removing NoxPlayerSetup Package...'
+  Remove-Item -Path $NoxPlayerSetupFile -Force
+
+  Write-Host 'Converting Commands...'
+  Get-ChildItem -Path $CommandPattern | ForEach-Object {
+    $FullName = $_.FullName
+    $Command = $NoxPlayerFolder + '\' + $_.BaseName + '.exe'
+    $ArgumentList = "/bat $FullName /exe $Command /invisible /icon $Icon"
+    Start-Process -Wait -NoNewWindow -ArgumentList $ArgumentList $Converter
   }
 
-  [String] GetNoxPlayerSetupFile() {
-    return $this.GetNoxPlayerFolder() + '\NoxPlayerSetup.exe'
-  }
-
-  [String] GetAssetFolder() {
-    return $PSScriptRoot + '\assets'
-  }
-
-  Run() {
-    $AssetFolder = $this.GetAssetFolder()
-    $Expand = $AssetFolder + '\7za.exe'
-    $Bignox = $AssetFolder + '\Bignox.7z'
-    $Converter = $AssetFolder + '\Bat_To_Exe_Converter.exe'
-    $CommandPattern = $AssetFolder + '\SecretRunScripts\*.cmd'
-
-    $NoxPlayerFolder = $this.GetNoxPlayerFolder()
-    $NoxPlayerSetupFile = $NoxPlayerFolder + '\NoxPlayerSetup.exe'
-    $NoxFolder = $NoxPlayerFolder + '\Nox'
-    $Icon = $NoxFolder + '\bin\res\shortcut5.ico'
-
-    Write-Host 'Creating NoxPlayer Folder...'
-    New-Item -ItemType Directory -Path $NoxPlayerFolder -Force
-
-    Write-Host 'Downloading NoxPlayerSetup Package...'
-    Receive-File $this.NoxPlayerSetupUri $NoxPlayerSetupFile
-
-    Write-Host 'Extracting NoxPlayerSetup Files...'
-    & $Expand x $NoxPlayerSetupFile -o"$NoxFolder" -y
-
-    Write-Host 'Extracting Bignox Files...'
-    & $Expand x $Bignox -o"$NoxPlayerFolder" -y
-
-    Write-Host 'Removing NoxPlayerSetup Package...'
-    Remove-Item -Path $NoxPlayerSetupFile -Force
-
-    Write-Host 'Converting Commands...'
-    Get-ChildItem -Path $CommandPattern | ForEach-Object -Process {
-      $Command = $this.GetNoxPlayerFolder() + '\' + $_.BaseName + '.exe'
-      & $Converter /bat $_.FullName /exe $Command /invisible /icon $Icon
-    }
-  }
-
-}
-
-function Show-Usage() {
   Write-Host
   Write-Host 'Copy The NoxPlayer Folder To Where Ever You Like, Like A USB-Stick'
   Write-Host 'To Run NoxPlayer Go To The NoxPlayer Folder And Run Nox.exe'
   Write-Host
-}
-
-function Main {
-  [Setup]::new().Run()
-  Show-Usage
 }
 
 Main
